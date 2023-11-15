4154d4801c7d9a2cc5c41aa9256537a0c8aefdcc
Vítor Rodrigues Tomé <vitorr661@gmail.com>
2023-11-14 22:16:17 -0300

    controle via websockets adicionado

--- control-car-backend/backend-esp/backend-esp.ino
+++ control-car-backend/backend-esp/backend-esp.ino
@@ -1,3 +1,6 @@
+#include <ESP8266WiFi.h>
+#include <WebSocketsServer.h>
+
 #define D0    16
 #define D1    5
 #define D2    4
@@ -13,22 +16,113 @@
 #define MOTOR2_PINO_POS    D4
 #define MOTOR2_PINO_NEG    D5
 
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+// Autenticação wi-fi - Coloque aqui a sua configuração
+const char* ssid     = "2G CASA 2";
+const char* password = "a3b3c3d3e3f3";
+
+// Tratamento de eventos dos dados que vêm do cliente 
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
+
+  switch (type) {
+    case WStype_DISCONNECTED:
+      break;
+
+    case WStype_CONNECTED:
+      { IPAddress ip = webSocket.remoteIP(num);
+        Serial.println(ip);
+      }
+      break;
+
+    case WStype_TEXT:
+      { String text = String((char *) &payload[0]);
+        Serial.println(text);
+        Serial.println(num);
+        Serial.println(type);
+
+        if (text == "FRONT") {
+          frente();
+        } else if (text == "LEFT") {
+          esquerda();
+        } else if (text == "RIGHT") {
+          direita();
+        } else if (text == "BACK") {
+          tras();
+        } else if (text == "OFF") {
+          para();
+        }
+      }
+      break;
+
+  }
+
+}
+
+void setaMotor(int motor1Positivo, int motor1Negativo, int motor2Positivo, int motor2Negativo) {
+  digitalWrite(MOTOR1_PINO_POS, motor1Positivo);
+  digitalWrite(MOTOR1_PINO_NEG, motor1Negativo);
+  digitalWrite(MOTOR2_PINO_POS, motor2Positivo);
+  digitalWrite(MOTOR2_PINO_NEG, motor2Negativo);
+}
+
+void para() {
+  setaMotor(0, 0, 0, 0);
+}
+
+void frente() {
+  setaMotor(255, 0, 255, 0);
+}
+
+void tras() {
+  setaMotor(0, 255, 0, 255);
+}
+
+void direita() {
+  setaMotor(0, 200, 200, 0);
+}
+
+void esquerda() {
+  setaMotor(200, 0, 0, 200);
+}
+
 void setup() {
+  Serial.begin(9600);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(". ");
+    delay(100);
+  }
+  Serial.println(WiFi.localIP());
+  webSocket.begin();
+  webSocket.onEvent(webSocketEvent);
+
   pinMode(MOTOR1_PINO_POS,OUTPUT);  
   pinMode(MOTOR1_PINO_NEG,OUTPUT);
   pinMode(MOTOR2_PINO_POS,OUTPUT);
   pinMode(MOTOR2_PINO_NEG,OUTPUT);
 
-  digitalWrite(MOTOR1_PINO_POS,LOW);
-  digitalWrite(MOTOR1_PINO_NEG,LOW);
-  digitalWrite(MOTOR2_PINO_POS,LOW);
-  digitalWrite(MOTOR2_PINO_NEG,LOW);
+  para();
 }
 
 void loop() {
-  analogWrite(MOTOR1_PINO_POS,200);
-  analogWrite(MOTOR1_PINO_NEG,0);
-  analogWrite(MOTOR2_PINO_POS,0);
-  analogWrite(MOTOR2_PINO_NEG,200);
+  webSocket.loop();
 
+  /*frente();
+  delay(5000);
+  para();
+  delay(2000);
+  tras();
+  delay(5000);
+  para();
+  delay(2000);
+  direita();
+  delay(5000);
+  para();
+  delay(2000);
+  esquerda();
+  delay(5000);
+  para();
+  delay(2000);*/
 }
---
+++ control-car-frontend/client.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("script carregado.");
+    
+    let connection = new WebSocket("ws://192.168.0.13:81", ['arduino']);
+
+    // escuta menssagem do ESP8266
+    connection.onmessage = function(messageEvent) {
+        console.log(messageEvent.data);
+    }
+
+    /**
+     * connection.send envia menssagem para o ESP8266
+     * onmousedown é um evento que ocorre quando um elemento recebe um clique
+     * onmouseup ocorre quanto o botão do mouse deixa de ser pressionado e remove o clique do elemento
+     *  */
+
+    const frontButton = document.getElementById("front");
+    frontButton.onmousedown = function(event) {
+        connection.send("FRONT");
+    }
+    frontButton.onmouseup = function(event) {
+        connection.send("OFF");
+    }
+
+    const leftButton = document.getElementById("left");
+    leftButton.onmousedown = function(event) {
+        connection.send("LEFT");
+    }
+    leftButton.onmouseup = function(event) {
+        connection.send("OFF");
+    }
+
+    const rightButton = document.getElementById("right");
+    rightButton.onmousedown = function(event) {
+        connection.send("RIGHT");
+    }
+    rightButton.onmouseup = function(event) {
+        connection.send("OFF");
+    }
+
+    const backButton = document.getElementById("back");
+    backButton.onmousedown = function(event) {
+        connection.send("BACK");
+    }
+    backButton.onmouseup = function(event) {
+        connection.send("OFF");
+    }
+
+});
